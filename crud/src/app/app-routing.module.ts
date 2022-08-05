313a1bb66b2ee564f3a4a28f9b074a907959452c
Bonolo <bonolo.solomon@dadevs.co.za>
2022-08-05 15:56:19 +0200

    still working on backend

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './Components/landing/landing.component';
 import { TableComponent } from './Components/table/table.component';
+import { UserComponent } from './Components/user/user.component';
 
 const routes: Routes = [ 
-  { path : "", component: TableComponent },
+  { path : "", component: LandingComponent },
+  { path : "table", component: TableComponent },
+  { path : "user", component: UserComponent },
 
 ];
  
