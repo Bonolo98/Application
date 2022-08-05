9fdd5bca707909455b3f0056fe2859a0d3beb755
Bonolo <bonolo.solomon@dadevs.co.za>
2022-08-05 15:58:26 +0200

    still working on backend

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserComponent } from './Components/user/user.component';
 import { LandingComponent } from './Components/landing/landing.component';
+import { AuthComponent } from './Components/auth/auth.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { LandingComponent } from './Components/landing/landing.component';
     TableComponent,
     UserComponent,
     LandingComponent,
+    AuthComponent,
     
 
   ],
